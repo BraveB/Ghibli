4249527573bf6d829f0eb67a047b096ac58a4dae
BraveB <bravogbeat@outlook.es>
2021-03-09 13:06:32 -0600

    New Release 20210309

---
+++ src/app/models/poster.model.ts
@@ -0,0 +1,7 @@
+export class PosterModel {
+  poster_path: string;
+
+  constructor(poster_path: string){
+    this.poster_path = poster_path;
+  }
+}
--- src/app/services/film.service.ts
+++ src/app/services/film.service.ts
@@ -3,19 +3,28 @@ import { HttpClient } from '@angular/common/http';
 import { FilmModel } from '../models/film.model';
 import { Observable } from 'rxjs';
 import { FilmDetailsModel } from '../models/filmDetails.model';
+import { PosterModel } from '../models/poster.model';
+import { environment } from '../../environments/environment'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmService {
   baseUrl: string;
+  moviesUrl: string;
   private favoriteFilms: FilmModel[] = [];
 
   constructor(private http: HttpClient) {
     this.baseUrl = "https://ghibliapi.herokuapp.com/films";
+    this.moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${environment.api_key}&query=`;
   }
 
   async addToFavorite(id: string){
+    const index = this.favoriteFilms.findIndex(film => film.id==id);
+    if(index > -1)
+      return;
+
     this.getFilmDetails(id)
     .then(film =>{
       film.isFavorite = true;
@@ -45,8 +54,19 @@ export class FilmService {
         const index = this.favoriteFilms.findIndex(favorite => favorite.id==film.id);
         film.isFavorite = index > -1;
     }})
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
     return formatedList;
+  }
+
+  async getPosterFormatedSource(title: string): Promise<PosterModel>{
+    let path: PosterModel = new PosterModel("");
+    this.getPosterSource(encodeURI(title))
+    .then(response=> {return path.poster_path = `https://image.tmdb.org/t/p/w342/${response.results[0].poster_path}`})
+    .catch(error=> console.error(error));
+    return path;
+  }
+  async getPosterSource(title: string): Promise<any>{
+    return await this.http.get<any>(`${this.moviesUrl}${title}`).toPromise();
   }
 
   getOptionList(): Observable<any[]> {
